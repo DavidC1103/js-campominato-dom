cf58e32fcf80e6b4feb7ad06b7ac9b41915a2e0d
David Calaiò <davidcjr11@gmail.com>
2023-03-30 19:57:20 +0200

    add endgame, control bomb, control points

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const btn = document.getElementById('btn')
 const container = document.querySelector('.dc-container')
 const level = document.getElementById('levels')
-
 const levelDiff = [100, 81, 49]
 const bombsNum = 16
 let arrBomb = []
 let points = 0
 const boxes = [];
+let controlBoxes = []
+var numBox = levelDiff [level.value]
 
 
 
@@ -14,9 +15,9 @@ const boxes = [];
 //creo il bottone e aggiungo tutte le funzioni all'inizio del gioco
 btn.addEventListener('click', function(){
     reset()
-    const numBox = levelDiff [level.value]
     genGrid(numBox)
     arrBomb = spawnBombs(numBox)
+    
 })
 
 //genero la griglia e la stamp su html
@@ -25,7 +26,7 @@ function genGrid(numBox){
     griglia.className = 'dc-container'
     for (let i = 1 ; i <= numBox; i++){
         let box = createBox(numBox, i);
-        boxes.push(box);
+        boxes.push(box)
         griglia.append(box)
     }
     container.append(griglia)
@@ -38,25 +39,47 @@ function createBox(numBox, id) {
     box.className = 'box'
     box.classList.add('dimbox' + numBox)
     box.aboxId = id;
-    console.log(id);
+    console.log(this.aboxId); 
     box.addEventListener('click', function() {
         console.log(this.aboxId);
         this.classList.add('clicked')
+        
+        if(controlBoxes.includes(this)){
+            console.log('questa box è inclusa');
+        }else{
+
+            if(arrBomb.includes(id)){
+                endgame(false)
+            }else{
+                controlBoxes.push(this)
+                console.log('sto dando punti',boxes);
+                points++
+                if(points === numBox - bombsNum){
+                    endgame(true)
+                }
+                console.log('punti',points);
+            }
+        }
     });
     return box;
 }
+
+
+
+
 //spawno le bombe
 function spawnBombs(numBox){
-//creo un array dove aggiungere le bombe
+    console.log('sto spwnadsadsa');
+    //creo un array dove aggiungere le bombe
     const bombs = [];
     console.log(bombs);
-//creo un ciclo while per dare valore a l'array per raggiungere il valore di bombsNum (16)    
+    //creo un ciclo while per dare valore a l'array per raggiungere il valore di bombsNum (16)    
     while(bombs.length < bombsNum){
-//se non è presente la pusho
+        //se non è presente la pusho
         const genBomb = getRandom(1,numBox )        
         if(!bombs.includes(genBomb)) bombs.push(genBomb)
     }
-//aggiungo la classe bomb a alla griglia    
+    //aggiungo la classe bomb a alla griglia    
     for (let i = 0; i < boxes.length; i++) {
         const box = boxes[i];
         box.addEventListener('click', function(){
@@ -70,13 +93,45 @@ function spawnBombs(numBox){
     return bombs
 }
 
+//fine gioco
+function endgame(win){
+    const fine = document.createElement('div')
+    container.append(fine)
+    console.log('fine');
+    const box = document.getElementsByClassName('box')
+    let winStr = ''
+    if(win){
+        winStr = `Hai vinto hai fatto un totale di ${points} su ${box.length - bombsNum}`
+        console.log(winStr);
+    }else{
+        winStr = `Hai perso hai fatto un totale di ${points} su ${box.length - bombsNum}`
+    }
+    
+    container.innerHTML = winStr;
+    return fine
+}
 
-
+function clickBox(button) {
+    button.classList.add('clicked')
+    if(boxes.includes(this.aboxId)){
+        endgame(false)
+    }else{
+        points++
+        this.removeEventListener('click', clickBox)
+        const box = document.getElementsByClassName('box')
+        if(points === box.length - bombsNum){
+            endgame(true)
+        }
+        console.log('punti',points);
+    }
+    
+}
 
 //resetto tutto
 function reset (){
     container.innerHTML = ''
     arrBomb = 0
+    points = 0
 }
 
 
